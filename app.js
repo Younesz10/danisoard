9c94d876df0f264c767a40f575f20046f72a9b40
younesz10 <yrz.ziewki18@gmail.com>
2023-08-13 00:51:30 -0400

    added a transition button to the next section

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ window.addEventListener("scroll", function () {
     navbarLinks.forEach((link) => link.classList.remove("white-links"));
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollButton = document.querySelector(".scroll-button");
+
+  scrollButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const targetSection = document.querySelector(this.getAttribute("href"));
+    const yOffset = -80; // Adjust this value based on your design
+    const y =
+      targetSection.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+    window.scrollTo({ top: y, behavior: "smooth" });
+  });
+});
